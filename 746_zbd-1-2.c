e01d77e91f01763af72cd1c50d4d17674002edea
BILIGTO\Билигто <biligto2012@gmail.com>
2018-05-20 18:40:45 +0700

    slasher

--- 746_zbd-1-2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-        int a,b,c;
-        scanf("%i%i",&a,&b);
-        c=a+b;
-        printf("%i\n",c);
- }
--- 746_zbd-1-3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-int main ()
-{
-        double x,y;
-        scanf("%lf%lf",&x,&y);
-        printf("%.4lf\n",pow( x,y) );
-        return 0;
-}
